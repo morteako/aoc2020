3bf7bfe3081208d24c03e26904e61f4f6ae93e93
Morten Kolstad <morteako@student.matnat.uio.no>
2020-12-06 13:06:05 +0100

    refactor 6

--- src/Day/Day06.hs
+++ src/Day/Day06.hs
@@ -1,19 +1,18 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Day.Day06 where
 
 import Data.List.Split (splitOn)
+import Data.Monoid
 import qualified Data.Set as Set
 
 parse :: String -> [[String]]
 parse = fmap lines . splitOn "\n\n"
 
-solve :: [[String]] -> Int
-solve = sum . fmap (Set.size . Set.fromList . concat)
-
-solve2 :: [[String]] -> Int
-solve2 = sum . fmap (Set.size . foldr1 Set.intersection . fmap Set.fromList)
+solve f = foldMap (Sum . Set.size . foldr1 f . fmap Set.fromList)
 
 run :: String -> IO ()
 run xs = do
   let parsed = parse xs
-  print $ solve parsed -- 6596
-  print $ solve2 parsed -- 3219
+  print $ solve Set.union parsed -- 6596
+  print $ solve Set.intersection parsed -- 3219
